a52109549e66b455a90498048aa251c14e3eedc1
eric-castillo05 <ericcastillo005@gmail.com>
2024-05-21 18:09:14 -0600

    feature: add models

---
+++ backend/src/main/java/models/Trabajador.java
@@ -0,0 +1,47 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "trabajador")
+@EntityListeners(AuditingEntityListener.class)
+public class trabajador {
+
+    @Column(name = "Usuario")
+    private String usuario;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Id
+    @Column(name = "Id_trab")
+    private int id;
+
+    @Column(name = "Nombre")
+    private String nombre;
+
+    @Column(name = "Sueldo_hr")
+    private float sueldo_hr;
+
+    @Column(name = "Fecha_ingreso")
+    private Date fecha_ingreso;
+
+    @Column(name = "Oficio")
+    private String oficio;
+
+    @Column(name = "RFC")
+    private String rfc;
+
+
+    @OneToMany(mappedBy = "trabajador")
+    private Set<Asignacion> asignaciones 
+}
